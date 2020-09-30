a174045106d6c419d5cd330ea40186ce6b4b0525
root <root@f1825c665844>
2020-09-30 01:27:24 +0000

    still on adventure

--- src/_02_loop_variables/_2_shiny_objects/ShinyObjects.java
+++ src/_02_loop_variables/_2_shiny_objects/ShinyObjects.java
@@ -1,35 +1,29 @@
 
 package _02_loop_variables._2_shiny_objects;
 
-import java.applet.AudioClip;
-
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.swing.JApplet;
-
+import javax.swing.JOptionPane;
 
 public class ShinyObjects {
 	public static void main(String[] args) {
-		
-		// 1. Call the method below to play the sound
+
+		// 1. Call the method below
 		// 2. Ask the user how many shiny objects they want
-		// 3. Play the sound that many times
+		// 3. Call the method that many times
 
-
-
-	}
-
-	/********************   Use this method. DON'T CHANGE THE CODE BELOW  ***********************/
-	
-	public static void playMisterZee() {
-		try {
-			AudioClip sound = JApplet.newAudioClip(ShinyObjects.class.getResource("shiny-objects.wav"));
-			sound.play();
-			Thread.sleep(3400);
-		} catch (Exception ex) {
-			ex.printStackTrace();
+		playMisterZee();
+		String zero = JOptionPane.showInputDialog(null, "How many shiny objects does Mr. Zee want?");
+		int num = Integer.parseInt(zero);
+		for (int i = 0; i < num; i++) {
+		playMisterZee();
 		}
 	}
 
+	/********************
+	 * Use this method. DON'T CHANGE THE CODE BELOW
+	 ***********************/
+
+	public static void playMisterZee() {
+		System.out.println("Mr. Zee requires shiny objects!");
+	}
 
 }
