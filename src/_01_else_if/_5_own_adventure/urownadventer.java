f81bd6c61d2ea62f6d6304db5f8a0af4343d3b9c
root <root@9a17f8c21916>
2020-10-07 01:26:37 +0000

    started bottles on the wall

--- src/_01_else_if/_5_own_adventure/urownadventer.java
+++ src/_01_else_if/_5_own_adventure/urownadventer.java
@@ -1,7 +1,30 @@
 package _01_else_if._5_own_adventure;
 
+import javax.swing.JOptionPane;
+
 public class urownadventer {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+
+		String option = JOptionPane.showInputDialog(null,
+				"You and your teamate are in a fight do you chose to run away and heal up or fight back?");
+
+		if (option.equalsIgnoreCase("I chose to fight back")) {
+			JOptionPane.showMessageDialog(null,
+					"You and your teamate hit one and lower thier HP by 199 they have 1 HP left");
+
+		} else if (option.equalsIgnoreCase("I chose to run away and heal")) {
+			JOptionPane.showMessageDialog(null, "Okay you run and hide to heal but you encounter another team!");
+			option = JOptionPane.showInputDialog(null,
+					"You and your teamate are in a fight do you chose to run away and heal up or fight back?");
+			if (option.equalsIgnoreCase("I chose to fight back")) {
+				JOptionPane.showMessageDialog(null,
+						"You and your teamate hit one and lower thier HP by 199 they have 1 HP left");
+			} else {
+				JOptionPane.showMessageDialog(null, "You succesfuly run and heal but, are now caught in the storm");
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Erorr 404");
+		}
+	}
+
 }
--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -6,8 +6,24 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		
-		// 1. Set the X position of the robot so that it starts on the left. 
+		int x = 100;
+		rob.hide();
+		rob.penDown();
+		rob.setSpeed(100);
+		rob.setRandomPenColor();
+		rob.setX(x);
+		for (int v = 0; v < 10; v++) {
+			for (int i = 0; i < 5; i++) {
+				rob.move(30);
+				rob.turn(144);
+				
+			}
+			rob.setAngle(0);
+			x= x + 50;
+			rob.setX(x);
+	
+		}
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
 
 		// 2. Make the robot draw a star shape. Hint: angle=144.
@@ -16,8 +32,7 @@ public class WalkOfFame {
 
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/RobotWalk
+		 * Make the robot draw a line of stars like this: http://bit.ly/RobotWalk
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
